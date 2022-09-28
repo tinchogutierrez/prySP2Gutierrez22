746a3b0eed9e10e1ca6502ed00dd415c35c6cc12
root <mgiraudo@ies21.edu.ar>
2022-09-28 12:34:30 -0300

    Agregar archivos de proyecto.

---
+++ frmPrincipal.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace prySP2Gutierrez
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.mrcTipo = new System.Windows.Forms.GroupBox();
+            this.lstPersonas = new System.Windows.Forms.ComboBox();
+            this.lstTipo = new System.Windows.Forms.ComboBox();
+            this.txtDias = new System.Windows.Forms.TextBox();
+            this.lblDias = new System.Windows.Forms.Label();
+            this.lblPersonas = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.mrcAdicionales = new System.Windows.Forms.GroupBox();
+            this.chkTelevision = new System.Windows.Forms.CheckBox();
+            this.chkHeladera = new System.Windows.Forms.CheckBox();
+            this.chkCocina = new System.Windows.Forms.CheckBox();
+            this.mrcPago = new System.Windows.Forms.GroupBox();
+            this.lblTarjetas = new System.Windows.Forms.Label();
+            this.lstTarjetas = new System.Windows.Forms.ComboBox();
+            this.optTarjeta = new System.Windows.Forms.RadioButton();
+            this.optEfectivo = new System.Windows.Forms.RadioButton();
+            this.mrcTitular = new System.Windows.Forms.GroupBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.lblDatos = new System.Windows.Forms.Label();
+            this.mrcTipo.SuspendLayout();
+            this.mrcAdicionales.SuspendLayout();
+            this.mrcPago.SuspendLayout();
+            this.mrcTitular.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mrcTipo
+            // 
+            this.mrcTipo.Controls.Add(this.lstPersonas);
+            this.mrcTipo.Controls.Add(this.lstTipo);
+            this.mrcTipo.Controls.Add(this.txtDias);
+            this.mrcTipo.Controls.Add(this.lblDias);
+            this.mrcTipo.Controls.Add(this.lblPersonas);
+            this.mrcTipo.Controls.Add(this.lblTipo);
+            this.mrcTipo.Location = new System.Drawing.Point(114, 74);
+            this.mrcTipo.Name = "mrcTipo";
+            this.mrcTipo.Size = new System.Drawing.Size(1112, 192);
+            this.mrcTipo.TabIndex = 0;
+            this.mrcTipo.TabStop = false;
+            this.mrcTipo.Text = "Tipo de cabaña";
+            this.mrcTipo.Enter += new System.EventHandler(this.mrcTipo_Enter);
+            // 
+            // lstPersonas
+            // 
+            this.lstPersonas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.lstPersonas.Enabled = false;
+            this.lstPersonas.FormattingEnabled = true;
+            this.lstPersonas.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.lstPersonas.Location = new System.Drawing.Point(579, 87);
+            this.lstPersonas.Name = "lstPersonas";
+            this.lstPersonas.Size = new System.Drawing.Size(121, 37);
+            this.lstPersonas.TabIndex = 6;
+            this.lstPersonas.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // lstTipo
+            // 
+            this.lstTipo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.lstTipo.FormattingEnabled = true;
+            this.lstTipo.Items.AddRange(new object[] {
+            "A",
+            "B"});
+            this.lstTipo.Location = new System.Drawing.Point(187, 87);
+            this.lstTipo.Name = "lstTipo";
+            this.lstTipo.Size = new System.Drawing.Size(121, 37);
+            this.lstTipo.TabIndex = 1;
+            this.lstTipo.SelectedIndexChanged += new System.EventHandler(this.lstTipo_SelectedIndexChanged);
+            // 
+            // txtDias
+            // 
+            this.txtDias.Enabled = false;
+            this.txtDias.Location = new System.Drawing.Point(936, 87);
+            this.txtDias.Name = "txtDias";
+            this.txtDias.Size = new System.Drawing.Size(107, 35);
+            this.txtDias.TabIndex = 5;
+            this.txtDias.TextChanged += new System.EventHandler(this.txtDias_TextChanged);
+            this.txtDias.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDias_KeyPress);
+            // 
+            // lblDias
+            // 
+            this.lblDias.AutoSize = true;
+            this.lblDias.Location = new System.Drawing.Point(850, 91);
+            this.lblDias.Name = "lblDias";
+            this.lblDias.Size = new System.Drawing.Size(61, 29);
+            this.lblDias.TabIndex = 4;
+            this.lblDias.Text = "Dias";
+            // 
+            // lblPersonas
+            // 
+            this.lblPersonas.AutoSize = true;
+            this.lblPersonas.Location = new System.Drawing.Point(421, 91);
+            this.lblPersonas.Name = "lblPersonas";
+            this.lblPersonas.Size = new System.Drawing.Size(115, 29);
+            this.lblPersonas.TabIndex = 2;
+            this.lblPersonas.Text = "Personas";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(88, 90);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(63, 29);
+            this.lblTipo.TabIndex = 0;
+            this.lblTipo.Text = "Tipo";
+            // 
+            // mrcAdicionales
+            // 
+            this.mrcAdicionales.Controls.Add(this.chkTelevision);
+            this.mrcAdicionales.Controls.Add(this.chkHeladera);
+            this.mrcAdicionales.Controls.Add(this.chkCocina);
+            this.mrcAdicionales.Enabled = false;
+            this.mrcAdicionales.Location = new System.Drawing.Point(114, 304);
+            this.mrcAdicionales.Name = "mrcAdicionales";
+            this.mrcAdicionales.Size = new System.Drawing.Size(366, 280);
+            this.mrcAdicionales.TabIndex = 1;
+            this.mrcAdicionales.TabStop = false;
+            this.mrcAdicionales.Text = "Adicionales";
+            // 
+            // chkTelevision
+            // 
+            this.chkTelevision.AutoSize = true;
+            this.chkTelevision.Location = new System.Drawing.Point(31, 197);
+            this.chkTelevision.Name = "chkTelevision";
+            this.chkTelevision.Size = new System.Drawing.Size(157, 33);
+            this.chkTelevision.TabIndex = 2;
+            this.chkTelevision.Text = "Televisión";
+            this.chkTelevision.UseVisualStyleBackColor = true;
+            this.chkTelevision.CheckedChanged += new System.EventHandler(this.chkTelevision_CheckedChanged);
+            // 
+            // chkHeladera
+            // 
+            this.chkHeladera.AutoSize = true;
+            this.chkHeladera.Location = new System.Drawing.Point(31, 132);
+            this.chkHeladera.Name = "chkHeladera";
+            this.chkHeladera.Size = new System.Drawing.Size(144, 33);
+            this.chkHeladera.TabIndex = 1;
+            this.chkHeladera.Text = "Heladera";
+            this.chkHeladera.UseVisualStyleBackColor = true;
+            this.chkHeladera.CheckedChanged += new System.EventHandler(this.chkHeladera_CheckedChanged);
+            // 
+            // chkCocina
+            // 
+            this.chkCocina.AutoSize = true;
+            this.chkCocina.Location = new System.Drawing.Point(31, 73);
+            this.chkCocina.Name = "chkCocina";
+            this.chkCocina.Size = new System.Drawing.Size(120, 33);
+            this.chkCocina.TabIndex = 0;
+            this.chkCocina.Text = "Cocina";
+            this.chkCocina.UseVisualStyleBackColor = true;
+            this.chkCocina.CheckedChanged += new System.EventHandler(this.chkCocina_CheckedChanged);
+            // 
+            // mrcPago
+            // 
+            this.mrcPago.Controls.Add(this.lblTarjetas);
+            this.mrcPago.Controls.Add(this.lstTarjetas);
+            this.mrcPago.Controls.Add(this.optTarjeta);
+            this.mrcPago.Controls.Add(this.optEfectivo);
+            this.mrcPago.Enabled = false;
+            this.mrcPago.Location = new System.Drawing.Point(549, 304);
+            this.mrcPago.Name = "mrcPago";
+            this.mrcPago.Size = new System.Drawing.Size(677, 280);
+            this.mrcPago.TabIndex = 3;
+            this.mrcPago.TabStop = false;
+            this.mrcPago.Text = "Forma de pago";
+            // 
+            // lblTarjetas
+            // 
+            this.lblTarjetas.AutoSize = true;
+            this.lblTarjetas.Enabled = false;
+            this.lblTarjetas.Location = new System.Drawing.Point(387, 170);
+            this.lblTarjetas.Name = "lblTarjetas";
+            this.lblTarjetas.Size = new System.Drawing.Size(101, 29);
+            this.lblTarjetas.TabIndex = 3;
+            this.lblTarjetas.Text = "Tarjetas";
+            // 
+            // lstTarjetas
+            // 
+            this.lstTarjetas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.lstTarjetas.Enabled = false;
+            this.lstTarjetas.FormattingEnabled = true;
+            this.lstTarjetas.Items.AddRange(new object[] {
+            "Card Red",
+            "Card Green",
+            "Card Blue"});
+            this.lstTarjetas.Location = new System.Drawing.Point(392, 213);
+            this.lstTarjetas.Name = "lstTarjetas";
+            this.lstTarjetas.Size = new System.Drawing.Size(255, 37);
+            this.lstTarjetas.TabIndex = 2;
+            this.lstTarjetas.SelectedIndexChanged += new System.EventHandler(this.lstTarjetas_SelectedIndexChanged);
+            // 
+            // optTarjeta
+            // 
+            this.optTarjeta.AutoSize = true;
+            this.optTarjeta.Location = new System.Drawing.Point(68, 170);
+            this.optTarjeta.Name = "optTarjeta";
+            this.optTarjeta.Size = new System.Drawing.Size(120, 33);
+            this.optTarjeta.TabIndex = 1;
+            this.optTarjeta.TabStop = true;
+            this.optTarjeta.Text = "Tarjeta";
+            this.optTarjeta.UseVisualStyleBackColor = true;
+            this.optTarjeta.CheckedChanged += new System.EventHandler(this.optTarjeta_CheckedChanged);
+            // 
+            // optEfectivo
+            // 
+            this.optEfectivo.AutoSize = true;
+            this.optEfectivo.Location = new System.Drawing.Point(68, 98);
+            this.optEfectivo.Name = "optEfectivo";
+            this.optEfectivo.Size = new System.Drawing.Size(129, 33);
+            this.optEfectivo.TabIndex = 0;
+            this.optEfectivo.TabStop = true;
+            this.optEfectivo.Text = "Efectivo";
+            this.optEfectivo.UseVisualStyleBackColor = true;
+            this.optEfectivo.CheckedChanged += new System.EventHandler(this.optEfectivo_CheckedChanged);
+            // 
+            // mrcTitular
+            // 
+            this.mrcTitular.Controls.Add(this.txtTelefono);
+            this.mrcTitular.Controls.Add(this.txtNombre);
+            this.mrcTitular.Controls.Add(this.lblTelefono);
+            this.mrcTitular.Controls.Add(this.lblNombre);
+            this.mrcTitular.Enabled = false;
+            this.mrcTitular.Location = new System.Drawing.Point(114, 612);
+            this.mrcTitular.Name = "mrcTitular";
+            this.mrcTitular.Size = new System.Drawing.Size(1112, 212);
+            this.mrcTitular.TabIndex = 4;
+            this.mrcTitular.TabStop = false;
+            this.mrcTitular.Text = "Titular de la Reserva";
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Enabled = false;
+            this.txtTelefono.Location = new System.Drawing.Point(230, 141);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(723, 35);
+            this.txtTelefono.TabIndex = 3;
+            this.txtTelefono.TextChanged += new System.EventHandler(this.txtTelefono_TextChanged);
+            this.txtTelefono.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTelefono_KeyPress);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(230, 68);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(723, 35);
+            this.txtNombre.TabIndex = 2;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(50, 147);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(110, 29);
+            this.lblTelefono.TabIndex = 1;
+            this.lblTelefono.Text = "Telefono";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(50, 68);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(101, 29);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Enabled = false;
+            this.btnAceptar.Location = new System.Drawing.Point(991, 858);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(235, 48);
+            this.btnAceptar.TabIndex = 5;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // lblDatos
+            // 
+            this.lblDatos.AutoSize = true;
+            this.lblDatos.Location = new System.Drawing.Point(1352, 94);
+            this.lblDatos.Name = "lblDatos";
+            this.lblDatos.Size = new System.Drawing.Size(0, 29);
+            this.lblDatos.TabIndex = 6;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(1846, 1049);
+            this.Controls.Add(this.lblDatos);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.mrcTitular);
+            this.Controls.Add(this.mrcPago);
+            this.Controls.Add(this.mrcAdicionales);
+            this.Controls.Add(this.mrcTipo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "frmPrincipal";
+            this.Tag = "";
+            this.Text = "Reserva de cabañas";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.mrcTipo.ResumeLayout(false);
+            this.mrcTipo.PerformLayout();
+            this.mrcAdicionales.ResumeLayout(false);
+            this.mrcAdicionales.PerformLayout();
+            this.mrcPago.ResumeLayout(false);
+            this.mrcPago.PerformLayout();
+            this.mrcTitular.ResumeLayout(false);
+            this.mrcTitular.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox mrcTipo;
+        private System.Windows.Forms.ComboBox lstPersonas;
+        private System.Windows.Forms.ComboBox lstTipo;
+        private System.Windows.Forms.TextBox txtDias;
+        private System.Windows.Forms.Label lblDias;
+        private System.Windows.Forms.Label lblPersonas;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.GroupBox mrcAdicionales;
+        private System.Windows.Forms.CheckBox chkTelevision;
+        private System.Windows.Forms.CheckBox chkHeladera;
+        private System.Windows.Forms.CheckBox chkCocina;
+        private System.Windows.Forms.GroupBox mrcPago;
+        private System.Windows.Forms.Label lblTarjetas;
+        private System.Windows.Forms.ComboBox lstTarjetas;
+        private System.Windows.Forms.RadioButton optTarjeta;
+        private System.Windows.Forms.RadioButton optEfectivo;
+        private System.Windows.Forms.GroupBox mrcTitular;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label lblDatos;
+    }
+}
+
---
+++ frmPrincipal.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prySP2Gutierrez
+{
+    public partial class frmPrincipal : Form
+    {
+        //DECLARO VARIABLES//
+        
+
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+       
+        private void mrcTipo_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtDias.Enabled = true;
+            if (lstPersonas.Text == "0" )
+            {
+                txtDias.Enabled = false;
+            }
+        }
+
+        private void chkCocina_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lstTipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstPersonas.Enabled = true;
+            if (lstTipo.Text == "" ) 
+            {
+                
+            }
+            if (lstTipo.SelectedItem == "A")
+            {
+                lstPersonas.Items.Clear();
+                lstPersonas.Items.Add(1);
+                lstPersonas.Items.Add(2);
+                lstPersonas.Items.Add(3);
+                lstPersonas.Items.Add(4);
+            }
+            if (lstTipo.SelectedItem == "B")
+            {
+                lstPersonas.Items.Clear();
+                lstPersonas.Items.Add(1);
+                lstPersonas.Items.Add(2);
+                lstPersonas.Items.Add(3);
+                lstPersonas.Items.Add(4); 
+                lstPersonas.Items.Add(5); 
+                lstPersonas.Items.Add(6);   
+                lstPersonas.Items.Add(7);   
+                lstPersonas.Items.Add(8);
+
+            }
+
+        }
+
+        private void txtDias_TextChanged(object sender, EventArgs e)
+        {
+            mrcAdicionales.Enabled = true ; mrcPago.Enabled = true ;    
+            // optEfectivo.Enabled=true; optTarjeta.Enabled=true;
+            // chkCocina.Enabled = true; chkHeladera.Enabled = true; chkTelevision.Enabled = true; 
+            if (txtDias.Text == "")
+            {
+                mrcAdicionales.Enabled= false ; mrcPago.Enabled = false ;   
+                // optEfectivo.Enabled = false; optTarjeta.Enabled = false;
+                // chkCocina.Enabled = false; chkHeladera.Enabled = false; chkTelevision.Enabled = false;
+            }
+        }
+
+        private void chkHeladera_CheckedChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void chkTelevision_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void optTarjeta_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            if (optTarjeta.Checked == true)
+            {
+                lstTarjetas.Enabled = true;
+                lblTarjetas.Enabled = true;  
+            }
+            else
+            {
+                lstTarjetas.Enabled = false;
+                lblTarjetas.Enabled = false;
+            }
+            if (optTarjeta.Checked == true && lstTarjetas.SelectedIndex != -1)
+            {
+                mrcTitular.Enabled = true;
+            }
+            
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            txtTelefono.Enabled = true;
+            if (txtNombre.Text == "")
+            {
+                txtTelefono.Enabled = false; 
+            }
+        }
+
+        private void optEfectivo_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            mrcTitular.Enabled = true;
+            if (optEfectivo.Checked == false)
+            {
+                mrcTitular.Enabled = false;
+            }
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text != "" && txtTelefono.Text != "")
+            {
+                lblDatos.Text = "Tipo: " + lstTipo.Text;
+                lblDatos.Text += "\nPersonas : " + lstPersonas.Text;
+                lblDatos.Text += "\nDias: " + txtDias.Text;
+                lblDatos.Text += "\nAdicionales: ";
+            }
+            if (chkCocina.Checked == true)
+            {
+                lblDatos.Text += "\n- Cocina: Si";
+            }
+            else
+            {
+                lblDatos.Text += "\n- Cocina: No";
+            }
+            if (chkHeladera.Checked == true)
+            {
+                lblDatos.Text += "\n- Heladera: Si";
+            }
+            else
+            {
+                lblDatos.Text += "\n- Heladera: No";
+            }
+            if (chkTelevision.Checked == true)
+            {
+                lblDatos.Text += "\n- Television: Si";
+            }
+            else
+            {
+                lblDatos.Text += "\n- Television: No";
+            }
+            if (optEfectivo.Checked == true)
+            {
+                lblDatos.Text += "\nForma de Pago: Efectivo";
+            }
+            if (optTarjeta.Checked == true)
+            {
+                lblDatos.Text += "\nFroma de Pago: Tarjeta" + lstTarjetas.SelectedItem; 
+                    
+
+            }
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+                                            // Cargar datos iniciales segun pdf //
+            // lstTipo.SelectedIndex = 0;
+            // lstPersonas.SelectedIndex = 0;
+            // txtDias.Text = "1";
+        }
+
+        private void txtDias_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Ingrese únicamente números");
+            }
+        }
+
+        private void txtTelefono_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Ingrese únicamente números");
+            }
+        }
+
+        private void txtTelefono_TextChanged(object sender, EventArgs e)
+        {
+            btnAceptar.Enabled = true;
+            if (txtTelefono.Text == "")
+            {
+                btnAceptar.Enabled = false;
+            }
+        }
+
+        private void lstTarjetas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtNombre.Enabled =true;  
+            mrcTitular.Enabled = true;
+
+        }
+    }
+    
+}
